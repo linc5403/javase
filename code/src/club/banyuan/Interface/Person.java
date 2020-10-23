e5051d7274f1b74a4585c33e6c8a113b261a6625
lin chuan <linch1982@gmail.com>
2020-10-23 16:40:45 +0800

    update

--- code/src/club/banyuan/Interface/Person.java
+++ code/src/club/banyuan/Interface/Person.java
@@ -13,11 +13,12 @@ public interface Person {
 
     // 缺省方法 - 如果实现类不重新该方法, 则使用该方法
     default String getName(){
-        return "";
-    };
+        return "Lucy";
+    }
 
     // 静态方法
     // 静态方法只能通过接口名称进行调用， 而不能通过实现类的类名调用
+    // 因此下面这个方法只能通过 Person.echoMaxAge() 来进行调用
     static void echoMaxAge() {
         System.out.println(MAX_AGE);
     }
